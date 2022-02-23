a80aca2e1736b8f7c7c9a0fbf5e9beebb29b0c0e
Jan Schering <j.schering@web.de>
2022-02-23 20:40:13 +0100

    some cleanup of leftovers

--- test.py
+++
@@ -1,9 +0,0 @@
-import gym
-import pybulletgym
-env = gym.make('InvertedPendulumMuJoCoEnv-v0')
-env.reset()
-print(f"OBS: {env.observation_space} ACT: {env.action_space}")
-for _ in range(1000):
-    _, reward, done, _ = env.step(env.action_space.sample())
-    env.render('human')
-env.close()
